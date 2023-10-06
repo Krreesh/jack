9e581ab4cad6de87313d0920f77bbc2f3652c645
Krreesh <cloudshell-user@ip-10-4-125-32.ec2.internal>
2023-10-06 08:51:13 +0000

    Revert "httpd"
    This reverts commit d50c042e6fb12159709a9e4511723fafda93a0b8.

--- httpd.sh
+++
@@ -1,3 +0,0 @@
-PORT_443=$(netstat -an | grep 443 | wc -l)
-aws cloudwatch put-metric-data --metric-name PORT_443_AVAILABILITY --dimensions Instance=i-0255e296e993b6df1 --namespace "port443" --value $
-echo "Hello"
